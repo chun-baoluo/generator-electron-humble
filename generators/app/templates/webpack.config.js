ea000d8542de0ddf201f185c481af99445a0dc9a
Pavel Bakharev <baharevpavel@gmail.com>
2017-02-26 22:50:39 +0700

    Updated to webpack 2.0, attempt to implement live reload

--- generators/app/templates/webpack.config.js
+++ generators/app/templates/webpack.config.js
@@ -4,6 +4,10 @@ var ExtractTextPlugin = require('extract-text-webpack-plugin');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 var config = {
+    devServer: {
+        inline: true
+    },
+
     entry: {
         polyfills: './dev/polyfills.ts',
         vendor: './dev/vendor.ts',
@@ -11,14 +15,14 @@ var config = {
     },
 
     resolve: {
-        extensions: ['', '.ts', '.js', <% if(cssPreprocessor == 'Stylus') { %>'.styl'<% } %><% if(cssPreprocessor == 'Less') { %>'.less'<% } %><% if(cssPreprocessor == 'Sass') { %>'.scss'<% } %>]
+        extensions: ['*', '.ts', '.js', <% if(cssPreprocessor == 'Stylus') { %>'.styl'<% } %><% if(cssPreprocessor == 'Less') { %>'.less'<% } %><% if(cssPreprocessor == 'Sass') { %>'.scss'<% } %>]
     },
 
     module: {
         loaders: [
             {
                 test: /\.ts$/,
-                loader: 'ts'
+                loader: 'ts-loader'
             },<% if(templateEngine == true) { %>
             {
                 test: /\.(jade|pug)$/,
@@ -35,19 +39,24 @@ var config = {
             },<% if(cssPreprocessor == 'Stylus') { %>
             {
                 test: /\.styl$/,
-                loader: ExtractTextPlugin.extract('style-loader', 'css-loader!stylus-loader')
+                loader: ExtractTextPlugin.extract({ fallback: 'style-loader', use: 'css-loader!stylus-loader' })
             }<% } %><% if(cssPreprocessor == 'Less') { %>
             {
                 test: /\.less$/,
-                loader: ExtractTextPlugin.extract('style-loader', 'css-loader!less-loader')
+                loader: ExtractTextPlugin.extract({ fallback: 'style-loader', use: 'css-loader!less-loader' })
             }<% } %><% if(cssPreprocessor == 'Sass') { %>
             {
                 test: /\.scss$/,
-                loader: ExtractTextPlugin.extract('style-loader', 'css-loader!sass-loader')
+                loader: ExtractTextPlugin.extract({ fallback: 'style-loader', use: 'css-loader!sass-loader' })
             }<% } %>
         ]
     },
 
+    node: {
+        __dirname: false,
+        __filename: false
+    },
+    
     plugins: [
         new webpack.optimize.CommonsChunkPlugin({
             name: ['app', 'vendor', 'polyfills']
@@ -63,19 +72,15 @@ module.exports = webpackMerge(config, {
     devtool: 'source-map',
 
     output: {
-        path: './app/res/',
-        publicPath: './',
-        filename: '[name].[hash].js',
+        path: __dirname + '/app/res/',
+        //publicPath: './',
+        publicPath: 'http://localhost:8080/',
+        filename: '[name].js',
         chunkFilename: '[id].chunk.js'
     },
 
-    htmlLoader: {
-        minimize: false
-    },
-
     plugins: [
-        new webpack.NoErrorsPlugin(),
-        new webpack.optimize.DedupePlugin(),
+        new webpack.NoEmitOnErrorsPlugin(),
         new webpack.optimize.UglifyJsPlugin({
             compress: {
                 warnings: false
@@ -84,6 +89,6 @@ module.exports = webpackMerge(config, {
                 keep_fnames: true
             }
         }),
-        new ExtractTextPlugin('[name].[hash].css')
+        new ExtractTextPlugin('[name].css')
     ]
 });
